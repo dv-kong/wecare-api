1f211d6fa9982800a85fe5be2dce82ed916ec50b
dv-kong <vincent.kong@outlook.com>
2022-02-26 21:40:06 +0100

    refactored error.ts, logger.ts, deleting middlewares.ts

--- src/helpers/error.ts
+++ src/helpers/error.ts
@@ -1,14 +1,41 @@
-import { logger } from "../config/middlewares";
-/**
- * @func ApiError Generate personalized error
- * @param statusCode error code
- * @param message error message
- */
+import { NextFunction, Response } from 'express';
+import { Request } from 'express';
+import { logger } from '../middlewares';
+
 class ApiError extends Error {
-  statusCode: number; // inherit from Error class
+  public statusCode;
   constructor(statusCode: number, message: string) {
-    super(message);
+    super();
     this.statusCode = statusCode;
+    this.message = message;
   }
 }
-export default ApiError;
+
+const handleError = (err: ApiError, req: Request, res: Response, next: NextFunction) => {
+  const { message } = err;
+  const statusCode = (err.statusCode) ? err.statusCode : 500;
+
+  logger.log(statusCode, err);
+  res.status(statusCode).json({
+    statusCode,
+    message
+  });
+}
+
+export { ApiError, handleError };
+
+
+// import { logger } from "../config/middlewares";
+// /**
+//  * @func ApiError Generate personalized error
+//  * @param statusCode error code
+//  * @param message error message
+//  */
+// class ApiError extends Error {
+//   statusCode: number; // inherit from Error class
+//   constructor(statusCode: number, message: string) {
+//     super(message);
+//     this.statusCode = statusCode;
+//   }
+// }
+// export default ApiError;
