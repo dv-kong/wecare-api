d8e90db237fdad9f9b2631afa36f6fccdd936c43
dv-kong <vincent.kong@outlook.com>
2022-02-26 21:49:07 +0100

    npm i csurf, updated .env, .env.example

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-  app_port: process.env.APP_PORT,
+  app_port: process.env.APP_PORT!,
   db_port: Number(process.env.TYPEORM_PORT),
   db_name: process.env.TYPEORM_DATABASE,
   db_user: process.env.TYPEORM_USERNAME,
@@ -12,7 +12,7 @@ const config = {
   db_entities: process.env.TYPEORM_ENTITIES,
   db_migration: process.env.TYPEORM_MIGRATIONS,
   db_migrationDir: process.env.TYPEORM_MIGRATIONS_DIR,
-  jwt_secret: process.env.JWT_SECRET
+  jwt_secret: process.env.JWT_SECRET!
 };
 
 export default config;
