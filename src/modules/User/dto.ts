3e7a1583238848962601a5b1c418a6a0def4c5a3
dv-kong <vincent.kong@outlook.com>
2022-02-26 21:30:01 +0100

    refactored dto.ts

--- src/modules/User/dto.ts
+++ src/modules/User/dto.ts
@@ -1,3 +1,20 @@
+
+class UserDTO {
+
+    public id;
+    public email;
+    constructor({ id, email }: { id: number, email: string }) {
+        this.id = id;
+        this.email = email;
+    }
+
+}
+
+export default UserDTO;
+
+
+// Legacy code
+
 // DTO
 // A data transfer object (DTO) is an object that carries data between processes.
 // You can use this technique to facilitate communication between two systems (like an API and your server) without potentially exposing sensitive information.
@@ -7,45 +24,49 @@
 // Filter the data that will be sent to client
 // Helps hide the data
 
-class UserDTO {
 
 
-    //TODO (A REFACTO) Rendre private les propriétés et déclarer des getters et setters
-    public email: string;
-    public password: string;
-    public first_name: string;
-    public last_name: string;
-    public postal_code: string;
-    public address: string;
-    public gender: string;
-    public city: string;
-    public phone_number: string;
-    public social_security_number: string;
 
-    constructor({
-        email,
-        password,
-        first_name,
-        last_name,
-        postal_code,
-        address,
-        gender,
-        city,
-        phone_number,
-        social_security_number
-    }: UserDTO) {
-        this.email = email;
-        this.password = password;
-        this.first_name = first_name;
-        this.last_name = last_name;
-        this.postal_code = postal_code;
-        this.address = address;
-        this.gender = gender;
-        this.city = city;
-        this.phone_number = phone_number;
-        this.social_security_number = social_security_number
-    }
-}
 
-export default UserDTO;
+// class UserDTO {
+
+
+//     //TODO (A REFACTO) Rendre private les propriétés et déclarer des getters et setters
+//     public email: string;
+//     public password: string;
+//     public first_name: string;
+//     public last_name: string;
+//     public postal_code: string;
+//     public address: string;
+//     public gender: string;
+//     public city: string;
+//     public phone_number: string;
+//     public social_security_number: string;
+
+//     constructor({
+//         email,
+//         password,
+//         first_name,
+//         last_name,
+//         postal_code,
+//         address,
+//         gender,
+//         city,
+//         phone_number,
+//         social_security_number
+//     }: UserDTO) {
+//         this.email = email;
+//         this.password = password;
+//         this.first_name = first_name;
+//         this.last_name = last_name;
+//         this.postal_code = postal_code;
+//         this.address = address;
+//         this.gender = gender;
+//         this.city = city;
+//         this.phone_number = phone_number;
+//         this.social_security_number = social_security_number
+//     }
+// }
+
+// export default UserDTO;
 
