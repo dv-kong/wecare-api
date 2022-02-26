21bce0617bee5e0715116b73eb95841a5e4f89cb
dv-kong <vincent.kong@outlook.com>
2022-02-26 21:06:21 +0100

    server.ts refacto

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,59 +1,37 @@
-import { Server, Post } from '@overnightjs/core'
-import { Application } from 'express';
+import { Response } from 'express';
+import { Request } from 'express';
+import { handleError } from '../helpers/error';
+import { Server } from '@overnightjs/core'
 
-class App {
+class App extends Server {
 
-    private http: Application; // private
+    constructor(routes: Array<Object>, middlewares: Object) {
+        super();
 
-    constructor(http) {
-        this.http = http;
+        this.initializeMiddlewares(middlewares);
+        super.addControllers(routes);
+        this.initializeErrorHandler();
     }
 
-/** hover over the function to see the documentation
- * @func middlewares for each middleware, server will use the good one
- * @param middleware middleware function take the middleware in argument
- * 
-*/
-middlewares(middlewares) {
-    
-    // console.log(`serverjs -> middlewares`, middlewares);
-    for (const key in middlewares) {
-
-       this.http.use(middlewares[key]); 
-
+    initializeMiddlewares(middlewares: any) {
+        for (const key in middlewares) {
+            if (key === 'csrf') {
+                this.app.get('/csrf', middlewares[key], (req: Request | any, res: Response) => {
+                    res.status(200).json(req.csrfToken());
+                })
+            }
+            else
+                this.app.use(middlewares[key]);
+        }
     }
-}
 
-/**
- * @func routes server will use each route contained in the routes object
- * @param routes
- * @TODO param (routes)
- */
-
-routes(routes) {
-
-    for(const path in routes) {
-        this.http.use(path, routes[path]);
+    initializeErrorHandler() {
+        this.app.use(handleError);
     }
-    
-}
 
-/** 
- * @func errorHandler make the server the error handler
- * @param errorHandler custom class inheriting from Error
- */
-errorHandler(errorHandler) {
-    this.http.use(errorHandler);
-}
-/**
- * @func start Start the server with the port in argument
- * @param port Value located in the .env file
- */
-start(port: string){
-    this.http.listen(port, () => {
-        console.log(`Server started on port: http://localhost:${port}`);
-    });
-}
+    listen(port: string) {
+        this.app.listen(port, async () => console.log(`application started on port : ${port}`));
+    }
 }
 
 export default App;
