160439df28eb5029ec87fbaedbeaa2c2bcb0bb06
dv-kong <vincent.kong@outlook.com>
2022-03-16 17:33:23 +0100

    fix: changed typeORM host to localhost (was db)

--- src/modules/User/interfaces/IUserRepository.ts
+++ src/modules/User/interfaces/IUserRepository.ts
@@ -1,26 +1,12 @@
 // import User from "../model";
-<<<<<<< HEAD
-
-// export default interface IUserRepository {
-//     findByEmail(email: string): Promise<User>
-//     create(user: User): Promise<User>
-//     login(email: string): Promise<User>
-//     update(jwtTokens: string, email: string): void
-//     delete(id: string): void
-//     findById(id: string): Promise<User>
-// }
-
-export default {}
-=======
 import UserDTO from "../dto";
 import { User } from '../entity'
 
 export default interface IUserRepository {
-    findAll(): Promise<User[]>;
-    addNew(userEntity: any): Promise<User>;
-    findByEmail(userEntity: any): Promise<User | undefined>;
-    findById(id: string): Promise<User | undefined>;
-    deleteById(id: string): Promise<void>;
-    compareHash(password: string, hash: string): Promise<boolean>;
-  }
->>>>>>> testing
+  findAll(): Promise<User[]>;
+  addNew(userEntity: any): Promise<User>;
+  findByEmail(userEntity: any): Promise<User | undefined>;
+  findById(id: string): Promise<User | undefined>;
+  deleteById(id: string): Promise<void>;
+  compareHash(password: string, hash: string): Promise<boolean>;
+}
