4c2463b2fb8d3e9e60429b9f8fa8dbed460d9364
Ali BELHACENE <abelhacene@wemanity.com>
2022-03-17 20:05:01 +0100

    feat: add in memory db for integration testing

--- tests/mocks/users.ts
+++ tests/mocks/users.ts
@@ -3,8 +3,7 @@ import { User } from "../../src/modules/User/entity";
 
 // UserDTO
 const mockaroo: any = [{
-    "id": 1,
-    "role": "Construction Expeditor",
+    "role": "user",
     "email": "dbarnsdale0@shop-pro.jp",
     "email_validated": false,
     "password": "TyGWQnI",
@@ -20,8 +19,7 @@ const mockaroo: any = [{
     "access_token": "3be02966bc0b54a323483cbb38fc2a691340ebf4",
     "refresh_token": "3be02966bc0b54a323483cbb38fc2a691340ebf4"
   }, {
-    "id": 2,
-    "role": "Estimator",
+    "role": "user",
     "email": "cjodkowski1@tripadvisor.com",
     "email_validated": true,
     "password": "RZYXDao8b4",
@@ -30,15 +28,14 @@ const mockaroo: any = [{
     "gender": "Female",
     "address": "48 Marcy Junction",
     "city": "Carania",
-    "postal_code": null,
+    "postal_code": "75000",
     "phone_number": "552-559-9903",
     "social_security_number": "02-056-0452",
     "banned": false,
     "access_token": "3be02966bc0b54a323483cbb38fc2a691340ebf4",
     "refresh_token": "3be02966bc0b54a323483cbb38fc2a691340ebf4"
   }, {
-    "id": 3,
-    "role": "Construction Foreman",
+    "role": "user",
     "email": "hklosser2@fc2.com",
     "email_validated": true,
     "password": "ZnHOPaznIFpW",
@@ -54,8 +51,7 @@ const mockaroo: any = [{
     "access_token": "3be02966bc0b54a323483cbb38fc2a691340ebf4",
     "refresh_token": "3be02966bc0b54a323483cbb38fc2a691340ebf4"
   }, {
-    "id": 4,
-    "role": "Construction Worker",
+    "role": "user",
     "email": "ibull3@bloglines.com",
     "email_validated": false,
     "password": "8F9B1J",
@@ -64,15 +60,14 @@ const mockaroo: any = [{
     "gender": "Female",
     "address": "33242 Boyd Road",
     "city": "Pasirsongket Dua",
-    "postal_code": null,
+    "postal_code": "75000",
     "phone_number": "282-154-8762",
     "social_security_number": "61-985-6368",
     "banned": true,
     "access_token": "3be02966bc0b54a323483cbb38fc2a691340ebf4",
     "refresh_token": "3be02966bc0b54a323483cbb38fc2a691340ebf4"
   }, {
-    "id": 5,
-    "role": "Engineer",
+    "role": "user",
     "email": "asturley4@pcworld.com",
     "email_validated": false,
     "password": "Jw6Wmc",
@@ -81,7 +76,7 @@ const mockaroo: any = [{
     "gender": "Female",
     "address": "1308 Del Mar Center",
     "city": "Dongxi",
-    "postal_code": null,
+    "postal_code": "75000",
     "phone_number": "726-328-1669",
     "social_security_number": "04-928-9745",
     "banned": true,
