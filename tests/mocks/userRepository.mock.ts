4d8c530756ccc3e9f7dd8655f4be394f8d769adc
Ali BELHACENE <abelhacene@wemanity.com>
2022-03-17 22:50:35 +0100

    test: add unit testing on register

--- tests/mocks/userRepository.mock.ts
+++ tests/mocks/userRepository.mock.ts
@@ -2,10 +2,15 @@ import IUserRepository from "../../src/modules/User/interfaces/IUserRepository";
 import { User } from "../../src/modules/User/entity";
 import users from "./users";
 import { createMock } from "ts-auto-mock";
+import UserService from "../../src/modules/User/service";
 
 const mockedRepository = createMock<IUserRepository>({
   findAll: async (): Promise<User[]> => {
     return await users;
+  },
+  addNew: async (user: any): Promise<User> => {
+    await users.push(user);
+    return users[users.length];
   },
 });
 
